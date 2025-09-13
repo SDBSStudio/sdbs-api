4baba4b7c5070be251a52d01e350ddd81c844f87
SDBSStudio <133013801+SDBSStudio@users.noreply.github.com>
2025-09-13 22:45:45 +0700

    Update playercount.js

--- api/playercount.js
+++ api/playercount.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET');
+
   const { id } = req.query;
 
   try {
